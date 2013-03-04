9865fa0f487f7e45b7320bb2d07974b3c388c378
Alessandro Ferreira Leite <alessandro.leite@alessandro.cc>
2013-03-04 18:50:49 +0100

    refactored Frequency for use jscience library

---
+++ core/src/main/java/greenapi/core/model/data/Energy.java
@@ -0,0 +1,87 @@
+package greenapi.core.model.data;
+
+import java.math.BigDecimal;
+
+import javax.measure.DecimalMeasure;
+import javax.measure.Measure;
+import javax.measure.quantity.Power;
+import javax.measure.unit.SI;
+import javax.measure.unit.Unit;
+
+import com.google.common.base.Preconditions;
+
+public final class Energy implements Data<Measure<BigDecimal, Power>>, Comparable<Energy> {
+
+	/**
+	 * Serial code version <code>serialVersionUID</code>
+	 */
+	private static final long serialVersionUID = 2718289241623424983L;
+
+	private Measure<BigDecimal, Power> value;
+
+	public Energy(BigDecimal value) {
+		this(value, SI.MEGA(SI.WATT));
+	}
+	
+	public Energy(BigDecimal value, Unit<Power> unit) {
+		this.value = DecimalMeasure.valueOf(value, unit);
+	}
+
+	public static Energy fromPowerInMegaWatt(BigDecimal power) {
+		return new Energy(power);
+	}
+
+	public static Energy fromPowerInMegaWatt(double power) {
+		return fromPowerInMegaWatt(BigDecimal.valueOf(power));
+	}
+
+	public static Energy fromPowerInJoule(double joule, long durationInMilis) {
+		Preconditions.checkArgument(durationInMilis > 0);
+		return new Energy(BigDecimal.valueOf(joule / (durationInMilis / 1000)));
+	}
+
+	@Override
+	public Measure<BigDecimal, Power> value() {
+		return this.value;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Energy other = (Energy) obj;
+		if (value == null) {
+			if (other.value != null) {
+				return false;
+			}
+		} else if (!value.equals(other.value)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return this.value().toString();
+	}
+
+	@Override
+	public int compareTo(Energy other) {
+		return this.value().compareTo(other.value());
+	}
+}
--- core/src/main/java/greenapi/core/model/data/Frequency.java
+++ core/src/main/java/greenapi/core/model/data/Frequency.java
@@ -22,30 +22,50 @@
  */
 package greenapi.core.model.data;
 
+import java.math.BigDecimal;
 
-public final class Frequency implements Data<Long>, Comparable<Frequency> {
+import javax.measure.DecimalMeasure;
+import javax.measure.Measure;
+import javax.measure.unit.SI;
+import javax.measure.unit.Unit;
+
+public final class Frequency implements
+		Data<Measure<BigDecimal, javax.measure.quantity.Frequency>>,
+		Comparable<Frequency> {
 
 	/**
 	 * Serial code version <code>serialVersionUID</code>
 	 */
-	private static final long serialVersionUID = 361164863478257017L;
-
-	private final long value;
+	private static final long serialVersionUID = -1779064340006691832L;
 
 	private static final Long UNKNOWN = 0L;
 
-	public static final Frequency NULL_FREQUENCY = new Frequency(UNKNOWN);
+	public static final Frequency NULL_FREQUENCY = newFrequencyInMhz(UNKNOWN);
 
-	public Frequency(Long value) {
-		this.value = (value == null ? UNKNOWN : value);
+	private final Measure<BigDecimal, javax.measure.quantity.Frequency> value;
+
+	public Frequency(long valueInMhz) {
+		this(valueInMhz, SI.MEGA(SI.HERTZ));
 	}
-	
-	public static Frequency valueOf(Long value) {
-		return new Frequency(value);
+
+	public Frequency(long value, Unit<javax.measure.quantity.Frequency> unit) {
+		this.value = DecimalMeasure.valueOf(BigDecimal.valueOf(value), unit);
+	}
+
+	public static Frequency newFrequencyInMhz(long valueInMhz) {
+		return new Frequency(valueInMhz);
+	}
+
+	public static Frequency newFrequencyInGhz(long valueInGhz) {
+		return new Frequency(valueInGhz, SI.GIGA(SI.HERTZ));
+	}
+
+	public static Frequency newFrequencyInHertz(long valueInHertz) {
+		return new Frequency(valueInHertz, SI.HERTZ);
 	}
 
 	@Override
-	public Long value() {
+	public Measure<BigDecimal, javax.measure.quantity.Frequency> value() {
 		return this.value;
 	}
 
@@ -55,42 +75,55 @@ public final class Frequency implements Data<Long>, Comparable<Frequency> {
 	 * @return The frequency value in GHz.
 	 */
 	public double inGhz() {
-		return this.value() / 1000d;
+		return this.value().doubleValue(SI.GIGA(SI.HERTZ));
 	}
-	
+
+	/**
+	 * Returns the frequency value in MHz.
+	 * 
+	 * @return The frequency value in MHz.
+	 */
+	public double inMhz() {
+		return this.value().doubleValue(SI.MEGA(SI.HERTZ));
+	}
+
 	@Override
 	public int compareTo(Frequency other) {
 		return this.value().compareTo(other.value());
 	}
 
 	@Override
+	public String toString() {
+		return this.value().toString();
+	}
+
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (int) (value ^ (value >>> 32));
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
 		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-
-		if (obj == null)
+		}
+		if (obj == null) {
 			return false;
-
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()) {
 			return false;
-
+		}
 		Frequency other = (Frequency) obj;
-		if (value != other.value)
+		if (value == null) {
+			if (other.value != null) {
+				return false;
+			}
+		} else if (!value.equals(other.value)) {
 			return false;
-
+		}
 		return true;
-	}
-	
-	@Override
-	public String toString() {
-		return this.value().toString();
 	}
 }
